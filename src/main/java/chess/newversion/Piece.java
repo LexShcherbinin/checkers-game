5415314a794223f6cb855885a982b75a28d3589e
lexsh <Aleksandr.Scherbinin@mvideo.ru>
2023-08-10 21:21:08 +0300

    Составил полный перечень ходов в Piece

--- src/main/java/chess/newversion/Piece.java
+++ src/main/java/chess/newversion/Piece.java
@@ -1,7 +1,87 @@
 package chess.newversion;
 
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_1;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_2;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_3;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_4;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_5;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_6;
+import static chess.enums.Moves.BISHOP_DOWN_LEFT_7;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_1;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_2;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_3;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_4;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_5;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_6;
+import static chess.enums.Moves.BISHOP_DOWN_RIGHT_7;
+import static chess.enums.Moves.BISHOP_UP_LEFT_1;
+import static chess.enums.Moves.BISHOP_UP_LEFT_2;
+import static chess.enums.Moves.BISHOP_UP_LEFT_3;
+import static chess.enums.Moves.BISHOP_UP_LEFT_4;
+import static chess.enums.Moves.BISHOP_UP_LEFT_5;
+import static chess.enums.Moves.BISHOP_UP_LEFT_6;
+import static chess.enums.Moves.BISHOP_UP_LEFT_7;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_1;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_2;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_3;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_4;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_5;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_6;
+import static chess.enums.Moves.BISHOP_UP_RIGHT_7;
+import static chess.enums.Moves.KING_CASTLING_LEFT;
+import static chess.enums.Moves.KING_CASTLING_RIGHT;
+import static chess.enums.Moves.KING_DOWN;
+import static chess.enums.Moves.KING_DOWN_LEFT;
+import static chess.enums.Moves.KING_DOWN_RIGHT;
+import static chess.enums.Moves.KING_LEFT;
+import static chess.enums.Moves.KING_RIGHT;
+import static chess.enums.Moves.KING_UP;
+import static chess.enums.Moves.KING_UP_LEFT;
+import static chess.enums.Moves.KING_UP_RIGHT;
 import static chess.enums.Moves.KNIGHT_1;
 import static chess.enums.Moves.KNIGHT_2;
+import static chess.enums.Moves.KNIGHT_3;
+import static chess.enums.Moves.KNIGHT_4;
+import static chess.enums.Moves.KNIGHT_5;
+import static chess.enums.Moves.KNIGHT_6;
+import static chess.enums.Moves.KNIGHT_7;
+import static chess.enums.Moves.KNIGHT_8;
+import static chess.enums.Moves.PAWN_BLACK_DOWN_1;
+import static chess.enums.Moves.PAWN_BLACK_DOWN_2;
+import static chess.enums.Moves.PAWN_BLACK_DOWN_LEFT;
+import static chess.enums.Moves.PAWN_BLACK_DOWN_RIGHT;
+import static chess.enums.Moves.PAWN_WHITE_UP_1;
+import static chess.enums.Moves.PAWN_WHITE_UP_2;
+import static chess.enums.Moves.PAWN_WHITE_UP_LEFT;
+import static chess.enums.Moves.PAWN_WHITE_UP_RIGHT;
+import static chess.enums.Moves.ROOK_DOWN_1;
+import static chess.enums.Moves.ROOK_DOWN_2;
+import static chess.enums.Moves.ROOK_DOWN_3;
+import static chess.enums.Moves.ROOK_DOWN_4;
+import static chess.enums.Moves.ROOK_DOWN_5;
+import static chess.enums.Moves.ROOK_DOWN_6;
+import static chess.enums.Moves.ROOK_DOWN_7;
+import static chess.enums.Moves.ROOK_LEFT_1;
+import static chess.enums.Moves.ROOK_LEFT_2;
+import static chess.enums.Moves.ROOK_LEFT_3;
+import static chess.enums.Moves.ROOK_LEFT_4;
+import static chess.enums.Moves.ROOK_LEFT_5;
+import static chess.enums.Moves.ROOK_LEFT_6;
+import static chess.enums.Moves.ROOK_LEFT_7;
+import static chess.enums.Moves.ROOK_RIGHT_1;
+import static chess.enums.Moves.ROOK_RIGHT_2;
+import static chess.enums.Moves.ROOK_RIGHT_3;
+import static chess.enums.Moves.ROOK_RIGHT_4;
+import static chess.enums.Moves.ROOK_RIGHT_5;
+import static chess.enums.Moves.ROOK_RIGHT_6;
+import static chess.enums.Moves.ROOK_RIGHT_7;
+import static chess.enums.Moves.ROOK_UP_1;
+import static chess.enums.Moves.ROOK_UP_2;
+import static chess.enums.Moves.ROOK_UP_3;
+import static chess.enums.Moves.ROOK_UP_4;
+import static chess.enums.Moves.ROOK_UP_5;
+import static chess.enums.Moves.ROOK_UP_6;
+import static chess.enums.Moves.ROOK_UP_7;
 
 import chess.Coordinates;
 import chess.enums.Colors;
@@ -80,12 +160,168 @@ public class Piece {
 
   public List<Function<IPieces, IPieces>> getActions() {
     switch (getName()) {
+      case PAWN -> {
+        if (color.equals(Colors.WHITE)) {
+          moveList = List.of(
+              PAWN_WHITE_UP_1,
+              PAWN_WHITE_UP_2,
+              PAWN_WHITE_UP_RIGHT,
+              PAWN_WHITE_UP_LEFT
+          );
+
+        } else {
+          moveList = List.of(
+              PAWN_BLACK_DOWN_1,
+              PAWN_BLACK_DOWN_2,
+              PAWN_BLACK_DOWN_RIGHT,
+              PAWN_BLACK_DOWN_LEFT
+          );
+        }
+      }
+
       case KNIGHT -> moveList = List.of(
-          KNIGHT_1
+          KNIGHT_1,
+          KNIGHT_2,
+          KNIGHT_3,
+          KNIGHT_4,
+          KNIGHT_5,
+          KNIGHT_6,
+          KNIGHT_7,
+          KNIGHT_8
       );
 
       case KING -> moveList = List.of(
-          KNIGHT_2
+          KING_UP,
+          KING_DOWN,
+          KING_RIGHT,
+          KING_LEFT,
+          KING_UP_RIGHT,
+          KING_UP_LEFT,
+          KING_DOWN_RIGHT,
+          KING_DOWN_LEFT,
+          KING_CASTLING_RIGHT,
+          KING_CASTLING_LEFT
+      );
+
+      case ROOK -> moveList = List.of(
+          ROOK_UP_1,
+          ROOK_UP_2,
+          ROOK_UP_3,
+          ROOK_UP_4,
+          ROOK_UP_5,
+          ROOK_UP_6,
+          ROOK_UP_7,
+          ROOK_DOWN_1,
+          ROOK_DOWN_2,
+          ROOK_DOWN_3,
+          ROOK_DOWN_4,
+          ROOK_DOWN_5,
+          ROOK_DOWN_6,
+          ROOK_DOWN_7,
+          ROOK_RIGHT_1,
+          ROOK_RIGHT_2,
+          ROOK_RIGHT_3,
+          ROOK_RIGHT_4,
+          ROOK_RIGHT_5,
+          ROOK_RIGHT_6,
+          ROOK_RIGHT_7,
+          ROOK_LEFT_1,
+          ROOK_LEFT_2,
+          ROOK_LEFT_3,
+          ROOK_LEFT_4,
+          ROOK_LEFT_5,
+          ROOK_LEFT_6,
+          ROOK_LEFT_7
+      );
+
+      case BISHOP -> moveList = List.of(
+          BISHOP_UP_RIGHT_1,
+          BISHOP_UP_RIGHT_2,
+          BISHOP_UP_RIGHT_3,
+          BISHOP_UP_RIGHT_4,
+          BISHOP_UP_RIGHT_5,
+          BISHOP_UP_RIGHT_6,
+          BISHOP_UP_RIGHT_7,
+          BISHOP_UP_LEFT_1,
+          BISHOP_UP_LEFT_2,
+          BISHOP_UP_LEFT_3,
+          BISHOP_UP_LEFT_4,
+          BISHOP_UP_LEFT_5,
+          BISHOP_UP_LEFT_6,
+          BISHOP_UP_LEFT_7,
+          BISHOP_DOWN_RIGHT_1,
+          BISHOP_DOWN_RIGHT_2,
+          BISHOP_DOWN_RIGHT_3,
+          BISHOP_DOWN_RIGHT_4,
+          BISHOP_DOWN_RIGHT_5,
+          BISHOP_DOWN_RIGHT_6,
+          BISHOP_DOWN_RIGHT_7,
+          BISHOP_DOWN_LEFT_1,
+          BISHOP_DOWN_LEFT_2,
+          BISHOP_DOWN_LEFT_3,
+          BISHOP_DOWN_LEFT_4,
+          BISHOP_DOWN_LEFT_5,
+          BISHOP_DOWN_LEFT_6,
+          BISHOP_DOWN_LEFT_7
+      );
+
+      case QUEEN -> moveList = List.of(
+          ROOK_UP_1,
+          ROOK_UP_2,
+          ROOK_UP_3,
+          ROOK_UP_4,
+          ROOK_UP_5,
+          ROOK_UP_6,
+          ROOK_UP_7,
+          ROOK_DOWN_1,
+          ROOK_DOWN_2,
+          ROOK_DOWN_3,
+          ROOK_DOWN_4,
+          ROOK_DOWN_5,
+          ROOK_DOWN_6,
+          ROOK_DOWN_7,
+          ROOK_RIGHT_1,
+          ROOK_RIGHT_2,
+          ROOK_RIGHT_3,
+          ROOK_RIGHT_4,
+          ROOK_RIGHT_5,
+          ROOK_RIGHT_6,
+          ROOK_RIGHT_7,
+          ROOK_LEFT_1,
+          ROOK_LEFT_2,
+          ROOK_LEFT_3,
+          ROOK_LEFT_4,
+          ROOK_LEFT_5,
+          ROOK_LEFT_6,
+          ROOK_LEFT_7,
+          BISHOP_UP_RIGHT_1,
+          BISHOP_UP_RIGHT_2,
+          BISHOP_UP_RIGHT_3,
+          BISHOP_UP_RIGHT_4,
+          BISHOP_UP_RIGHT_5,
+          BISHOP_UP_RIGHT_6,
+          BISHOP_UP_RIGHT_7,
+          BISHOP_UP_LEFT_1,
+          BISHOP_UP_LEFT_2,
+          BISHOP_UP_LEFT_3,
+          BISHOP_UP_LEFT_4,
+          BISHOP_UP_LEFT_5,
+          BISHOP_UP_LEFT_6,
+          BISHOP_UP_LEFT_7,
+          BISHOP_DOWN_RIGHT_1,
+          BISHOP_DOWN_RIGHT_2,
+          BISHOP_DOWN_RIGHT_3,
+          BISHOP_DOWN_RIGHT_4,
+          BISHOP_DOWN_RIGHT_5,
+          BISHOP_DOWN_RIGHT_6,
+          BISHOP_DOWN_RIGHT_7,
+          BISHOP_DOWN_LEFT_1,
+          BISHOP_DOWN_LEFT_2,
+          BISHOP_DOWN_LEFT_3,
+          BISHOP_DOWN_LEFT_4,
+          BISHOP_DOWN_LEFT_5,
+          BISHOP_DOWN_LEFT_6,
+          BISHOP_DOWN_LEFT_7
       );
 
     }
